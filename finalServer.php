6f84fe00c785f49332b76b7c611575fcb92ad0db
elisabettagabriele <46170718+elisabettagabriele@users.noreply.github.com>
2019-05-02 23:00:36 -0400

    Add files via upload

---
+++ finalServer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html lang = "en">
+    <head>
+		<title>Teacher's php</title>
+        
+	</head>
+<body>
+
+    <p id ="words"> </p>
+
+<?php
+echo "hello can anybody see this";
+    print_r($_FILES);
+
+
+?>
+    
+    
+    
+    </body>
+</html>
+    
